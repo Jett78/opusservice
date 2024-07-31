b79a5f9cbfb4687491e7d7b8c0f9047ccf9b9438
Jeet <jetdeula@gmail.com>
2024-07-31 10:37:23 +0545

    updated

--- src/app/Home/Chairmanmssg.tsx
+++ src/app/Home/Chairmanmssg.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { ImQuotesLeft } from "react-icons/im";
 import { ImQuotesRight } from "react-icons/im";
@@ -5,13 +6,16 @@ import { ImQuotesRight } from "react-icons/im";
 const Chairmanmssg: React.FC = () => {
   return (
     <main className="w-11/12 mx-auto ">
-
       <div className="mx-auto rounded-lg ">
-        <div className="flex flex-row-reverse items-end justify-center gap-0 ">
-          <section className="relative bg-gray-100 w-[35%] h-[35em]   px-6 py-10">
+        <div className="flex flex-row-reverse items-center justify-center gap-10 ">
+          <section className="relative  w-[40%] h-[35em]   px-6 py-10">
             <div className=" pb-10">
-            <h2 className="text-gradient text-2xl font-bold">Chairman's Message</h2>
-            <h3 className="text-3xl font-bold">Honest Services for Sustained Results</h3>
+              <h2 className="text-gradient text-2xl font-bold">
+                Chairmans Message
+              </h2>
+              <h3 className="text-4xl font-bold">
+                Honest Services for Sustained Results
+              </h3>
             </div>
             <p className="font-medium text-sm">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
@@ -28,22 +32,43 @@ const Chairmanmssg: React.FC = () => {
               amet consectetur adipisicing elit. Quos adipisci velit ratione
               similique, pariatur libero sit? Animi consequuntur totam fugiat,
             </p>
-            <h3 className="text-xl font-bold mt-6">John Doe</h3>
-            <p className="text-[12px] uppercase font-semibold">Chairman</p>
-            <div className="flex gap-4">
-              <li>icon</li>
-              <li>icon</li>
-              <li>icon</li>
+            <h3 className="text-xl font-bold mt-6">Alaxander Doe</h3>
+            <p className="text-[12px] font-semibold">Chairman</p>
+
+            <div className="flex gap-4 mt-4">
+              <Image
+                src="/icons/facebookicon.png"
+                width={30}
+                height={30}
+                alt="icon"
+              />
+              <Image
+                src="/icons/instaicon.png"
+                width={30}
+                height={30}
+                alt="icon"
+              />
+              <Image
+                src="/icons/whatsappicon.png"
+                width={30}
+                height={30}
+                alt="icon"
+              />
             </div>
-            <div className="absolute text-6xl top-32 opacity-5">
-            <ImQuotesLeft />
-          </div>
-          <div className="absolute text-6xl bottom-16 right-10 opacity-5">
-            <ImQuotesRight />
-          </div>
+
+            <div className="absolute text-6xl top-36 opacity-5">
+              <ImQuotesLeft />
+            </div>
+            <div className="absolute text-6xl bottom-28 right-10 opacity-5">
+              <ImQuotesRight />
+            </div>
           </section>
-          <section className="w-[50%] h-[72vh] ">
-            <img src="./chairman.jpg" alt="chairman" className="rounded-lg w-[60em] h-[35em] object-cover brightness-75" />
+          <section className="w-[40%]  ">
+            <img
+              src="./chairman.jpg"
+              alt="chairman"
+              className="rounded-lg w-[60em] h-[42em] object-cover brightness-75"
+            />
           </section>
         </div>
       </div>
