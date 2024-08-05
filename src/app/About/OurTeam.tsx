c303d18fc6f1e279d69fd4d011e623ea6929d4a7
Jeet <jetdeula@gmail.com>
2024-08-05 17:42:14 +0545

    changes

--- src/app/About/OurTeam.tsx
+++ src/app/About/OurTeam.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import Image from "next/image";
 import React, { useState } from "react";
 import Slider from "react-slick";
@@ -25,7 +25,10 @@ const OurTeam = () => {
       <h2 className="font-extrabold text-2xl uppercase w-fit mx-auto">
         Meet Our <span className="text-gradient">Team</span>
       </h2>
-      <p className="font-medium text-md max-w-[60em] md:mx-auto mx-2 sm:text-l text-sm py-2 text-center"> Our team is equipped with a diverse range of certifications that validate our skills and expertise across various countries</p>
+      <p className="font-medium text-md max-w-[60em] md:mx-auto mx-2 sm:text-l text-sm py-2 text-center">
+        Our team is equipped with a diverse range of certifications that
+        validate our skills and expertise across various countries
+      </p>
 
       <div className="grid lg:grid-cols-2 grid-cols-1 place-items-center md:w-7/12 w-11/12 mx-auto justify-center gap-10 my-20 group cursor-pointer">
         {teamdata.map((item, index) => (
@@ -62,7 +65,6 @@ const OurTeam = () => {
         ))}
       </div>
 
-      
       <div className="md:grid hidden lg:grid-cols-4 grid-cols-2 place-items-center w-10/12 mx-auto justify-center gap-10 my-20 group cursor-pointer">
         {teamdata2.map((item, index) => (
           <div
@@ -100,75 +102,41 @@ const OurTeam = () => {
 
       {/* slider for smaller screens */}
       <div className="cursor-pointer md:hidden block mx-6">
-      <Slider {...settings}>
-        {teamdata2.map((item, index) => (
-          <div
-            key={index}
-            className="relative group "
-            onMouseEnter={() => setHoveredIndex2(index)}
-            onMouseLeave={() => setHoveredIndex2(null)}
-          >
-            <div className="overflow-hidden rounded-lg">
-              <Image
-                src={item.img}
-                alt="team"
-                width={700}
-                height={1000}
-                className={`h-[42vh] w-full object-cover ease-in-out duration-300 ${
-                  hoveredIndex2 === index ? "grayscale scale-105" : ""
-                }`}
-              />
-            </div>
+        <Slider {...settings}>
+          {teamdata2.map((item, index) => (
             <div
-              className={`bg-white w-full px-2 absolute bottom-0 border-b py-2 transition-opacity duration-300 ${
-                hoveredIndex2 === index ? "opacity-100" : "opacity-0"
-              }`}
+              key={index}
+              className="relative group "
+              onMouseEnter={() => setHoveredIndex2(index)}
+              onMouseLeave={() => setHoveredIndex2(null)}
             >
-              <div className="grid place-items-center whitespace-nowrap py-2 ">
-                <h2 className="font-bold text-xl">{item.name}</h2>
-                <h3 className="font-semibold  text-sm italic tracking-wide">
-                  {item.role}
-                </h3>
+              <div className="overflow-hidden rounded-lg">
+                <Image
+                  src={item.img}
+                  alt="team"
+                  width={700}
+                  height={1000}
+                  className={`h-[42vh] w-full object-cover ease-in-out duration-300 ${
+                    hoveredIndex2 === index ? "grayscale scale-105" : ""
+                  }`}
+                />
+              </div>
+              <div
+                className={`bg-white w-full px-2 absolute bottom-0 border-b py-2 transition-opacity duration-300 ${
+                  hoveredIndex2 === index ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                <div className="grid place-items-center whitespace-nowrap py-2 ">
+                  <h2 className="font-bold text-xl">{item.name}</h2>
+                  <h3 className="font-semibold  text-sm italic tracking-wide">
+                    {item.role}
+                  </h3>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-          </Slider>
+          ))}
+        </Slider>
       </div>
-      {/* <div className="flex flex-wrap justify-center gap-10 my-20 group cursor-pointer">
-        {teamdata3.map((item, index) => (
-          <div
-            key={index}
-            className="relative group"
-            onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
-          >
-            <div className="overflow-hidden rounded-lg">
-              <Image
-                src={item.img}
-                alt="team"
-                width={700}
-                height={1000}
-                className={`h-[50vh] w-[20em] object-cover ease-in-out duration-300 ${
-                  hoveredIndex === index ? "grayscale scale-105" : ""
-                }`}
-              />
-            </div>
-            <div
-              className={`bg-white w-full px-2 absolute bottom-0 border-b py-2 transition-opacity duration-300 ${
-                hoveredIndex === index ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              <div className="grid place-items-center whitespace-nowrap py-2 ">
-                <h2 className="font-bold text-xl">{item.name}</h2>
-                <h3 className="font-semibold  text-sm italic tracking-wide">
-                  {item.role}
-                </h3>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div> */}
     </main>
   );
 };
@@ -186,7 +154,6 @@ const teamdata = [
     img: "/team/team1.jpg",
     role: "Chief Marketting Officer",
   },
- 
 ];
 const teamdata2 = [
   {
@@ -236,5 +203,4 @@ const teamdata3 = [
     img: "/team/team1.jpg",
     role: "Chief Executive Officer",
   },
-
 ];
